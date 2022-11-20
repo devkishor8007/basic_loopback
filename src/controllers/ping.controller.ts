0cf42000e602d2bbcab7d2823f26e0a68bdc93ae
kishorkc <kishor8007kc@gmail.com>
2022-11-20 10:35:21 +0545

    first commit

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,82 @@
+import {inject} from '@loopback/core';
+import {
+  Request,
+  RestBindings,
+  get,
+  response,
+  ResponseObject,
+} from '@loopback/rest';
+
+/**
+ * OpenAPI response for ping()
+ */
+const PING_RESPONSE: ResponseObject = {
+  description: 'Ping Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'PingResponse',
+        properties: {
+          greeting: {type: 'string'},
+          date: {type: 'string'},
+          url: {type: 'string'},
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': {type: 'string'},
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+const ME_RESPONSE: ResponseObject = {
+  description: 'ME Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'MERespnse',
+        properties: {
+          name: {type: 'string'},
+          age: {type: 'number'},
+          date: {type: 'string'},
+        },
+      },
+    },
+  },
+};
+
+/**
+ * A simple controller to bounce back http requests
+ */
+export class PingController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+
+  // Map to `GET /ping`
+  @get('/ping')
+  @response(200, PING_RESPONSE)
+  ping(): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return {
+      greeting: 'Hello from LoopBack',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
+  }
+
+  @get('/me')
+  @response(200, ME_RESPONSE)
+  me(): object {
+    return {
+      name: "kishor",
+      age: 12,
+      date: new Date(),
+    }
+  }
+}
